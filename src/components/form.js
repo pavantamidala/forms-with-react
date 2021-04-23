904660508a96f6cbe02bb3cd6d4b2129f64cccb2
pavantamidala <pavantamidala023@gmail.com>
2021-04-23 11:37:37 +0530

    added styling

--- src/components/form.js
+++ src/components/form.js
@@ -2,32 +2,34 @@ import "./form-validation.css"
 import React from "react";
 import { useState } from "react";
 import clickHandle from './clickHandle';
+import TextField from '@material-ui/core/TextField';
 
 
 function FormComponent(props) {
     const [validation,setValidation] = useState({firstName:"",lastName:"",email:"",number:""})
     return (
         <form className="form"  onSubmit={(e) => clickHandle(e, props,validation,setValidation)}>
-            <label htmlFor="firstName">First Name:</label>
-            <input type="text" id="firstName" value={props.userObj.firstName} name="firstName" onChange={e => props.setUserObj({ ...props.userObj, "firstName": e.target.value })} /> <br />
+            
+            <TextField label="First Name" variant="outlined" type="text" id="firstName" value={props.userObj.firstName} name="firstName" onChange={e => props.setUserObj({ ...props.userObj, "firstName": e.target.value })} /> <br />
             <p className="validation firstName-validation"> {validation.firstName} </p>
 
 
-            <label htmlFor="lastName">Last Name:</label>
-            <input type="text" id="lastName" value={props.userObj.lastName} onChange={e => props.setUserObj({ ...props.userObj, "lastName": e.target.value })} name="lastName" /> <br />
+            
+            <TextField label="Last Name" variant="outlined" type="text" id="lastName" value={props.userObj.lastName} onChange={e => props.setUserObj({ ...props.userObj, "lastName": e.target.value })} name="lastName" /> <br />
             <p className="validation lastName-validation"> {validation.lastName} </p>
 
 
 
-            <label htmlFor="email">Email:</label>
-            <input type="email" name="email" id="email" value={props.userObj.email} onChange={(e) => props.setUserObj({ ...props.userObj, "email": e.target.value })} /> <br/>
+            
+            <TextField label="Email" variant="outlined" type="email" name="email" id="email" value={props.userObj.email} onChange={(e) => props.setUserObj({ ...props.userObj, "email": e.target.value })} /> <br/>
             <p className="validation email-validation"> {validation.email} </p>
 
 
-            <label htmlFor="number">Number:</label>
-            <input type="number" id="number" name="number" value={props.userObj.number} onChange={(e) => props.setUserObj({ ...props.userObj, number: e.target.value })} />
+            
+            <TextField label="Ph Number" variant="outlined" type="number" id="number" name="number" value={props.userObj.number} onChange={(e) => props.setUserObj({ ...props.userObj, number: e.target.value })} />
             <p className="validation number-validation"> {validation.number} </p>
-            <button className="submit-btn btn">Submit </button>
+            <button  className="submit-btn btn">Submit </button> 
+            
         </form>
     );
 }
